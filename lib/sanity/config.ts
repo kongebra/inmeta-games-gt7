f23c3cce42714094255d904eaa3779b09262dfc0
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-11-04 16:11:12 +0100

    feat: foo

--- lib/sanity/config.ts
+++ lib/sanity/config.ts
@@ -2,7 +2,7 @@ import { ClientConfig } from "next-sanity";
 
 const config: ClientConfig = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "jhhsx3kh",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "",
   apiVersion: "2021-10-21",
 
   useCdn: process.env.NODE_ENV === "production",

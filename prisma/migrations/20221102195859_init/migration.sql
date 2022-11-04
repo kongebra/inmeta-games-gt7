ead1919687358ee3349e13d25511d33fe049debc
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-11-04 16:27:41 +0100

    feat: what?

--- prisma/migrations/20221102195859_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Player" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL,
-    "min" INTEGER NOT NULL,
-    "sec" INTEGER NOT NULL,
-    "ms" INTEGER NOT NULL
-);

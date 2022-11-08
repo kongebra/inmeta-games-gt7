538f8fed52eea527ab4d75c7a52999b58cbeb782
Svein Are Danielsen <svein.danielsen@inmeta.no>
2022-11-08 15:02:57 +0100

    feat: snitttid

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo, useState } from "react";
 
 import { Score } from "@prisma/client";
 import { Box, Container, Flex, Heading, Stack, Text } from "@chakra-ui/react";
@@ -22,11 +22,7 @@ const PlayerImage: React.FC<PlayerImageProps> = ({ player, size = 32 }) => {
     (player?.image as any) || null,
     {
       imageBuilder: (builder) =>
-        builder
-          .crop("focalpoint")
-          .fit("crop")
-          .width(size)
-          .height(size),
+        builder.crop("focalpoint").fit("crop").width(size).height(size),
     }
   ) as any;
 
@@ -84,10 +80,10 @@ export default function Home() {
     return players.find((player) => player._id === playerId);
   };
 
-  const sortedScores = useMemo(() => scores.sort(sortScores), [
-    scores,
-    sortScores,
-  ]);
+  const sortedScores = useMemo(
+    () => scores.sort(sortScores),
+    [scores, sortScores]
+  );
 
   const bestLaptime = useMemo(() => {
     if (sortedScores.length > 0) {
@@ -113,60 +109,110 @@ export default function Home() {
     [bestLaptime]
   );
 
+  const averageTime = useMemo(() => {
+    if (scores.length === 0) {
+      return 0;
+    }
+
+    const timesInSec = scores.map(calculateTime);
+
+    return (
+      timesInSec.reduce((prev, curr) => prev + curr, 0) / timesInSec.length
+    );
+  }, [scores]);
+
+  const averageTimeMinSecMs = useMemo(() => {
+    const min = Math.floor(averageTime);
+    const sec = Math.floor((averageTime * 60) % 60);
+    const ms = Math.floor((averageTime * 60 * 1000) % 1000);
+
+    return { min, sec, ms };
+  }, [averageTime]);
+
   return (
     <>
       <Box minW={"100vw"} minH={"100vh"} bg={"gray.300"}>
         <Container maxW="container.lg" pt={4}>
-          <Heading textAlign={"center"} mb={8} fontSize={"4rem"}>
+          <Heading textAlign={"center"} fontSize={"4rem"} mb={4}>
             Top Gear - Inmeta Games
           </Heading>
 
           <Stack>
             {scores.sort(sortScores)?.map((score, index) => {
+              const currentTime = calculateTime(score);
+              const prevTime =
+                index === 0 ? -1 : calculateTime(scores[index - 1]);
+
               const timeBehindLeader = timeBehindBestTime(score);
 
+              const avgLine =
+                currentTime > averageTime && prevTime < averageTime;
+
               return (
-                <Flex
-                  key={score.id}
-                  px={4}
-                  py={2}
-                  bg={getBgPlacement(index)} // TODO: sjekk om noen har samme rundetid
-                  justify="space-between"
-                  align="center"
-                  fontSize="1.75rem"
-                  rounded="sm"
-                >
-                  <Flex align="center" gap={4} flex="1">
-                    <Box
-                      as="span"
-                      minW={8}
-                      fontFamily="fantasy"
-                      fontWeight="bold"
+                <React.Fragment key={score.id}>
+                  {avgLine ? (
+                    <Flex
+                      p={0.5}
+                      bg="gray.700"
+                      justify="center"
+                      alignItems="center"
                     >
-                      {index + 1}
-                    </Box>
-
-                    <PlayerImage player={getPlayer(score.playerId)} size={50} />
-                    <Text fontFamily="fantasy">
-                      {getPlayerName(score.playerId)}
-                    </Text>
-                  </Flex>
-
-                  <Flex fontFamily="monospace" alignItems="center" gap={4}>
-                    {timeBehindLeader === 0 ? (
-                      <span></span>
-                    ) : (
-                      <Text fontStyle="italic">
-                        {`+${timeBehindLeader.toFixed(3)}sec`}
+                      <Text
+                        color="white"
+                        fontSize="1.5rem"
+                        fontWeight="semibold"
+                      >
+                        Snittlinja: {averageTimeMinSecMs.min}.
+                        {String(averageTimeMinSecMs.sec).padStart(2, "0")}:
+                        {String(averageTimeMinSecMs.ms).padStart(3, "0")}
                       </Text>
-                    )}
+                    </Flex>
+                  ) : null}
 
-                    <strong>
-                      {score.min}.{String(score.sec).padStart(2, "0")}:
-                      {String(score.ms).padStart(3, "0")}
-                    </strong>
+                  <Flex
+                    px={4}
+                    py={2}
+                    bg={getBgPlacement(index)} // TODO: sjekk om noen har samme rundetid
+                    justify="space-between"
+                    align="center"
+                    fontSize="1.75rem"
+                    rounded="sm"
+                  >
+                    <Flex align="center" gap={4} flex="1">
+                      <Box
+                        as="span"
+                        minW={8}
+                        fontFamily="fantasy"
+                        fontWeight="bold"
+                      >
+                        {index + 1}
+                      </Box>
+
+                      <PlayerImage
+                        player={getPlayer(score.playerId)}
+                        size={50}
+                      />
+                      <Text fontFamily="fantasy">
+                        {getPlayerName(score.playerId)}
+                      </Text>
+                    </Flex>
+
+                    <Flex fontFamily="monospace" alignItems="center" gap={4}>
+                      {timeBehindLeader === 0 ? (
+                        <span></span>
+                      ) : (
+                        <Text fontStyle="italic">
+                          {`+${timeBehindLeader.toFixed(3)}sec`}
+                        </Text>
+                      )}
+
+                      <strong>
+                        {score.min}.{String(score.sec).padStart(2, "0")}:
+                        {String(score.ms).padStart(3, "0")}
+                      </strong>
+                    </Flex>
                   </Flex>
-                </Flex>
+                </React.Fragment>
               );
             })}
           </Stack>
